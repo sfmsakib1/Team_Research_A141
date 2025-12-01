3207263537c854e2b2d024f83c043b7778aebaa9
haiderherts <ha24afi@herts.ac.uk>
2025-12-01 13:06:42 +0000

    add the person test for the data displaying pearson r, p-value, and number of rows.

--- script.R
+++ script.R
@@ -6,6 +6,7 @@
 
 df <- read.csv("~/Downloads/2019.csv", check.names = FALSE)
 
+#change column names for understanding
 dat <- data.frame(
   country   = df[["Country or region"]],
   happinessScore = df[["Score"]],
@@ -14,3 +15,17 @@ dat <- data.frame(
 #remove null values
 dat <- na.omit(dat)
 df
+
+# 2) Correlation test (Pearson)
+cor_out <- cor.test(dat$gdp, dat$happinessScore, method = "pearson")
+
+r <- unname(cor_out$estimate)
+#person r
+p <- cor_out$p.value
+#p-value
+n <- nrow(dat)
+#number of rows used in test
+
+cat("\n--- Correlation test ---\n")
+#print full test results
+print(cor_out)
