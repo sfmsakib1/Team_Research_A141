9246c72217556686ba9a52b7dd0f8b893fc31fc5
haiderherts <ha24afi@herts.ac.uk>
2025-12-01 13:35:58 +0000

    added the scatter plot with regression line with labels and some important information on the plot

--- script.R
+++ script.R
@@ -46,3 +46,51 @@ summary_table <- data.frame(
 
 cat("\n--- Summary table ---\n")
 print(summary_table)
+
+# ================================================
+# MAIN PLOT: Scatterplot with regression line
+# ================================================
+
+# Regression line fitting from a linear model
+fit <- lm(happinessScore ~ gdp, data=dat)
+
+# Plot with colors
+plot(dat$gdp, dat$happinessScore,
+     pch = 16,
+     col = rgb(0.1, 0.4, 0.9, 0.55),           # semi-transparent blue points
+     xlab = "GDP per capita",
+     ylab = "Happiness score",
+     main = "GDP per capita vs Happiness (2019)",
+     panel.first = grid(col = "grey85", lty = "dotted"))
+
+# use to draw the regression line
+abline(fit, lwd = 2, lty = 2, col = "firebrick")
+# red dashed line
+lines(lowess(dat$gdp, dat$happinessScore), lwd = 2, col = "darkgreen")  # green smooth
+
+# Add r, p, n in bottom-right
+txt <- paste0("Pearson r = ", round(r, 3),
+              "\np = ", format(p, scientific=TRUE, digits=2),
+              "\nn = ", n)
+usr <- par("usr")
+text(usr[2], usr[3], txt, adj = c(1, 0), cex = 0.9, col = "grey20")
+
+legend("topleft",
+       legend = c("Countries", "Regression line (lm)", "LOWESS smooth"),
+       pch = c(16, NA, NA),
+       lty = c(NA, 2, 1),
+       lwd = c(NA, 2, 3),
+       col = c(rgb(0.1, 0.4, 0.9, 0.55), "firebrick", "darkgreen"),
+       bty = "n")
+
+
+#Biggest outliers from the regression line 
+res <- resid(fit)
+out3 <- order(abs(res), decreasing = TRUE)[1:3]
+
+label_idx <- unique(c(out3))
+
+# Add labels slightly above/right of points
+text(dat$gdp[label_idx], dat$happinessScore[label_idx],
+     labels = dat$country[label_idx],
+     pos = 4, cex = 0.75, col = "black")
