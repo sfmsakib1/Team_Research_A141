14147f9ad1b47fdccd80a6c59130124ed1033782
haiderherts <ha24afi@herts.ac.uk>
2025-12-03 15:33:13 +0000

    update the scatter plot makes it more cleaner and readable by removing excessive information

--- script.R
+++ script.R
@@ -57,10 +57,10 @@ fit <- lm(happinessScore ~ gdp, data=dat)
 # Plot with colors
 plot(dat$gdp, dat$happinessScore,
      pch = 16,
-     col = rgb(0.1, 0.4, 0.9, 0.55),           # semi-transparent blue points
+     col = rgb(0.1, 0.4, 0.9, 0.55),  # semi-transparent blue points
      xlab = "GDP per capita",
      ylab = "Happiness score",
-     main = "GDP per capita vs Happiness (2019)",
+     main = "GDP per capita vs Happiness Score (2019)",
      panel.first = grid(col = "grey85", lty = "dotted"))
 
 # use to draw the regression line
@@ -85,15 +85,15 @@ legend("topleft",
 
 
 #Biggest outliers from the regression line 
-res <- resid(fit)
-out3 <- order(abs(res), decreasing = TRUE)[1:3]
+#res <- resid(fit)
+#out3 <- order(abs(res), decreasing = TRUE)[1:3]
 
-label_idx <- unique(c(out3))
+#label_idx <- unique(c(out3))
 
 # Add labels slightly above/right of points
-text(dat$gdp[label_idx], dat$happinessScore[label_idx],
-     labels = dat$country[label_idx],
-     pos = 4, cex = 0.75, col = "black")
+#text(dat$gdp[label_idx], dat$happinessScore[label_idx],
+#     labels = dat$country[label_idx],
+#    pos = 4, cex = 0.75, col = "black")
 
 
 # ----------------------------------------
