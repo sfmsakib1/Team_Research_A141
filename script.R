4c354dc81da328703af1a813acefc9a8778aed0c
Seikh Fuyad Sakib <ss24anp@herts.ac.uk>
2025-12-07 07:09:38 +0000

    Cleaned code and commented

--- script.R
+++ script.R
@@ -1,104 +1,31 @@
 
-# RQ: Is GDP per capita positively correlated with happiness score? (2019)
+# ----------------------------------------------------------
+# RQ: Is there a positive correlation between GDP per capita 
+#     and  the  Happiness Score in the  2019 World Happiness 
+#     Report?
+# ----------------------------------------------------------
 
+# ----------------------------------------------------------
+# 1) Load and prepare data
+# ----------------------------------------------------------
 
-# 1) Data preprocessing
+df <- read.csv("2019.csv", check.names = FALSE)
 
-df <- read.csv("~/Downloads/2019.csv", check.names = FALSE)
-
-#change column names for understanding
+# Change column names
 dat <- data.frame(
   country   = df[["Country or region"]],
-  happinessScore = df[["Score"]],
+  happiness = df[["Score"]],
   gdp       = df[["GDP per capita"]]
 )
-#remove null values
+#remove all null values
 dat <- na.omit(dat)
-df
 
-# 2) Correlation test (Pearson)
-cor_out <- cor.test(dat$gdp, dat$happinessScore, method = "pearson")
+dat
 
-r <- unname(cor_out$estimate)
-#person r
-p <- cor_out$p.value
-#p-value
-n <- nrow(dat)
-#number of rows used in test
+# ----------------------------------------------------------
+# 2) HISTOGRAMS (Check normality for choosing test)
+# ----------------------------------------------------------
 
-cat("\n--- Correlation test ---\n")
-#print full test results
-print(cor_out)
-
-
-# ---- Summary table-----
-summary_table <- data.frame(
-  n = n,
-  happiness_mean = mean(dat$happinessScore),
-  happiness_sd   = sd(dat$happinessScore),
-  happiness_min  = min(dat$happinessScore),
-  happiness_max  = max(dat$happinessScore),
-  gdp_mean = mean(dat$gdp),
-  gdp_sd   = sd(dat$gdp),
-  gdp_min  = min(dat$gdp),
-  gdp_max  = max(dat$gdp)
-)
-
-cat("\n--- Summary table ---\n")
-print(summary_table)
-
-# ------------------------------------------------
-# MAIN PLOT: Scatterplot with regression line
-# -------------------------------------------------
-
-# Regression line fitting from a linear model
-fit <- lm(happinessScore ~ gdp, data=dat)
-
-# Plot with colors
-plot(dat$gdp, dat$happinessScore,
-     pch = 16,
-     col = rgb(0.1, 0.4, 0.9, 0.55),  # semi-transparent blue points
-     xlab = "GDP per capita",
-     ylab = "Happiness score",
-     main = "GDP per capita vs Happiness Score (2019)",
-     panel.first = grid(col = "grey85", lty = "dotted"))
-
-# use to draw the regression line
-abline(fit, lwd = 2, lty = 2, col = "firebrick")
-# red dashed line
-lines(lowess(dat$gdp, dat$happinessScore), lwd = 2, col = "darkgreen")  # green smooth
-
-# Add r, p, n in bottom-right
-#txt <- paste0("Pearson r = ", round(r, 3),
-#              "\np = ", format(p, scientific=TRUE, digits=2),
-#             "\nn = ", n)
-#usr <- par("usr")
-#text(usr[2], usr[3], txt, adj = c(1, 0), cex = 0.9, col = "grey20")
-
-legend("topleft",
-       legend = c("Countries", "Regression line (lm)", "LOWESS smooth"),
-       pch = c(16, NA, NA),
-       lty = c(NA, 2, 1),
-       lwd = c(NA, 2, 3),
-       col = c(rgb(0.1, 0.4, 0.9, 0.55), "firebrick", "darkgreen"),
-       bty = "n")
-
-
-#Biggest outliers from the regression line 
-#res <- resid(fit)
-#out3 <- order(abs(res), decreasing = TRUE)[1:3]
-
-#label_idx <- unique(c(out3))
-
-# Add labels slightly above/right of points
-#text(dat$gdp[label_idx], dat$happinessScore[label_idx],
-#     labels = dat$country[label_idx],
-#    pos = 4, cex = 0.75, col = "black")
-
-
-# ----------------------------------------
-# SUPPLEMENTARY GRAPH: Histograms
-# -------------------------------------
 par(mfrow = c(1, 2))  # two plots side-by-side
 
 h <- hist(dat$gdp, breaks = 20, plot = FALSE)
@@ -125,11 +52,11 @@ text(x = h$mids,
 
 
 
-h2   <- hist(dat$happinessScore, breaks = 20, plot = FALSE)
+h2   <- hist(dat$happiness, breaks = 20, plot = FALSE)
 xmax <- max(h2$breaks)
 ymax <- max(h2$counts)
 
-hist(dat$happinessScore, breaks = h2$breaks,
+hist(dat$happiness, breaks = h2$breaks,
      xlim = c(2, xmax),
      ylim = c(0, ymax * 1.25),
      main = "Histogram of Happiness score ",
@@ -148,25 +75,92 @@ text(x = h2$mids,
 
 par(mfrow = c(1, 1))  # reset
 
+# ----------------------------------------------------------
+# 3) Scatterplot + Regression line
+# ----------------------------------------------------------
 
-#-------------------------------
-#.        contingency table
-#--------------------------------
+# Regression line fitting from a linear model
+fit <- lm(happiness ~ gdp, data=dat)
 
+# Plot with colors
+plot(dat$gdp, dat$happiness,
+     pch = 16,
+     col = rgb(0.1, 0.4, 0.9, 0.55),  # semi-transparent blue points
+     xlab = "GDP per capita",
+     ylab = "Happiness score",
+     main = "GDP per capita vs Happiness Score (2019)",
+     panel.first = grid(col = "grey85", lty = "dotted"))
 
+# use to draw the regression line
+abline(fit, lwd = 2, lty = 2, col = "firebrick")
+# red dashed line
+lines(lowess(dat$gdp, dat$happiness), lwd = 2, col = "darkgreen")  # green smooth
+
+legend("topleft",
+       legend = c("Countries", "Regression line (lm)", "LOWESS smooth"),
+       pch = c(16, NA, NA),
+       lty = c(NA, 2, 1),
+       lwd = c(NA, 2, 3),
+       col = c(rgb(0.1, 0.4, 0.9, 0.55), "firebrick", "darkgreen"),
+       bty = "n")
+
+# ------------------------------------------------------------
+# 4) Correlation Test: pearson (After checking histograms)
+# ------------------------------------------------------------
+
+cor_out <- cor.test(dat$gdp, dat$happiness, method = "pearson")
+
+r <- unname(cor_out$estimate)
+#person r
+p <- cor_out$p.value
+#p-value
+n <- nrow(dat)
+#number of rows used in test
+
+cat("\n--- Correlation test ---\n")
+#print full test results
+print(cor_out)
+
+cor_out <- cor.test(dat$gdp, dat$happiness, method = "pearson")
+print(cor_out)
+
+# ------------------------------------------------------------
+# 5) Summary statistics
+# ------------------------------------------------------------
+summary_table <- data.frame(
+  n = n,
+  happiness_mean = mean(dat$happiness),
+  happiness_sd   = sd(dat$happiness),
+  happiness_min  = min(dat$happiness),
+  happiness_max  = max(dat$happiness),
+  gdp_mean = mean(dat$gdp),
+  gdp_sd   = sd(dat$gdp),
+  gdp_min  = min(dat$gdp),
+  gdp_max  = max(dat$gdp)
+)
+
+cat("\n--- Summary table ---\n")
+print(summary_table)
+
+# ------------------------------------------------------------
+# 6) Contingency table
+# ------------------------------------------------------------
 
 # turned both numeric variables into 3 categories
 dat$gdp_cat <- cut(dat$gdp,
-                   breaks = quantile(dat$gdp, probs = c(0, 1/3, 2/3, 1), na.rm = TRUE),
+                   breaks = quantile(dat$gdp, 
+                                     probs = c(0, 1/3, 2/3, 1), na.rm = TRUE),
                    include.lowest = TRUE,
-                   labels = c("Low GDP", "Mid GDP", "High GDP"))
+                   labels = c("Low GDP", "Mid GDP", "High GDP")
+                   )
 
-dat$happy_cat <- cut(dat$happinessScore,
-                     breaks = quantile(dat$happinessScore, probs = c(0, 1/3, 2/3, 1), na.rm = TRUE),
+dat$happy_cat <- cut(dat$happiness,
+                     breaks = quantile(dat$happiness, 
+                                       probs = c(0, 1/3, 2/3, 1), na.rm = TRUE),
                      include.lowest = TRUE,
-                     labels = c("Low Happy", "Mid Happy", "High Happy"))
+                     labels = c("Low Happy", "Mid Happy", "High Happy")
+                     )
 
-# Contingency table
 tab <- table(dat$gdp_cat, dat$happy_cat)
 tab
 
